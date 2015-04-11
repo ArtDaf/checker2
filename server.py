cb80d390640ebec78fe0df79f6a6639fd0f77737
nexusriot <nexus.riot@gmail.com>
2015-04-11 04:23:29 +0300

    Created model Category

--- server.py
+++ server.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_TEST_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
 
@@ -14,11 +16,29 @@ class File(db.Model):
     display_name = db.Column(db.String(256), unique=False)
     url = db.Column(db.String(512), unique=False)
     hash = db.Column(db.String(36), unique=False)
+    date = db.Column(db.DateTime)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
+    category = db.relationship('Category',
+                backref=db.backref('files', lazy='dynamic'))
+
+    def __init__(self, display_name, url, hash, category, date=None):
+        self.display_name = display_name
+        self.url = url
+        self.hash = hash
+        if date is None:
+            date = datetime.utcnow()
+        self.category = category
 
 
 class Category(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(50), unique=False)
+
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return '<Category %r>' % self.name
 
 
 @app.route('/')
